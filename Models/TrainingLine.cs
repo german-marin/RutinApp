5b9110fd6088d540bb0e9bfc6b0a43a70c3f0afb
Germán <german.marin_acosta@contractors.roche.com>
2023-11-30 13:21:01 +0100

    subimos

---
+++ Models/TrainingLine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RutinApp.Models
+{
+    internal class TrainingLine
+    {
+        public int ID { get; set; }
+        public int IdExercise { get; set; }
+        public int IdTraining { get; set; }
+        public string Series { get; set; }
+        public string Repetition { get; set; }
+        public string Weight { get; set; }
+        public string Recovery { get; set; }
+        public string Others { get; set; }
+        public string Notes { get; set; }
+
+        public TrainingLine(int iD, int idExercise, int idTraining, string series, string repetition, string weight, string recovery, string others, string notes)
+        {
+            ID = iD;
+            IdExercise = idExercise;
+            IdTraining = idTraining;
+            Series = series;
+            Repetition = repetition;
+            Weight = weight;
+            Recovery = recovery;
+            Others = others;
+            Notes = notes;
+        }
+    }
+}
